3b8152880e5934761d71273092c1fccd46a3233e
Samm Kohistani <sk@apples-MacBook-Pro.local>
2023-08-27 19:59:02 +0100

    add gridlayout with clickable arrows to change images

--- src/Components/Properties/BoxList/BoxList.js
+++ src/Components/Properties/BoxList/BoxList.js
@@ -1,30 +1,60 @@
-import React from "react";
+import React, { useState } from 'react';
 import beaufort from './images/beaufort.png';
 import chelsea from './images/chelsea.jpg';
 import fulham from './images/fulham.jpeg';
 import kings from './images/kings.jpeg';
 import laundreyworks from './images/laundreyworks.jpeg';
 import woodberry from './images/woodberry.png';
+import { Grid, IconButton } from '@mui/material';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
-import { Grid } from '@mui/material';
+function ImageBox({ images }) {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-export default function BoxList() {
-    const images = [
-        {id: 1, src: beaufort},
-        {id: 2, src: chelsea},
-        {id: 3, src: fulham},
-        {id: 4, src: kings},
-        {id: 5, src: laundreyworks},
-        {id: 6, src: woodberry}
-    ];
+  const handlePrevImage = () => {
+    setCurrentImageIndex(currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1);
+  };
 
-    return (
-        <Grid container spacing={2} justifyContent={"center"} sx={{ padding:'10px 70px'}}>
-            {images.map((image) => (
-                <Grid key={image.id} item xs={12} sm={4} sx={{ display:'flex', justifyContent: 'center'}}>
-                <img src={image.src} style={{ width: '80%', height: '70%', marginTop: '150px' }}/>
-            </Grid>
-            ))}
+  const handleNextImage = () => {
+    setCurrentImageIndex(currentImageIndex === images.length - 1 ? 0 : currentImageIndex + 1);
+  };
+
+  return (
+    <div>
+    <img src={images[currentImageIndex]} alt={`Image ${currentImageIndex}`} style={{ width: '400px', height: '300px', position:'relative', paddingBottom:'50px', paddingTop:'50px'}} />
+      <IconButton onClick={handlePrevImage} style={{ position: 'absolute', top: '50%', left: '30px', transform: 'translateY(-50%)', zIndex: 2 }}>
+        <KeyboardArrowLeftIcon />
+      </IconButton>
+      <IconButton onClick={handleNextImage} style={{ position: 'absolute', top: '50%', right: '15px', transform: 'translateY(-50%)', zIndex: 2 }}>
+        <KeyboardArrowRightIcon />
+      </IconButton>
+    </div>
+  );
+}
+
+
+
+
+const listings = [
+  { images: [beaufort, chelsea, fulham, kings, woodberry, laundreyworks] },
+  { images: [chelsea, beaufort, kings, fulham, laundreyworks, woodberry] },
+  { images: [woodberry, laundreyworks, kings, fulham, beaufort, woodberry] },
+  { images: [beaufort, chelsea, fulham, kings, woodberry, laundreyworks] },
+  { images: [woodberry, fulham, kings, laundreyworks, laundreyworks, woodberry] },
+  { images: [laundreyworks, laundreyworks, kings, fulham, beaufort, woodberry] }
+];
+
+function BoxList() {
+  return (
+    <Grid container spacing={2} sx={{ padding: '20px 70px' , position: 'relative', top:'150px'}}>
+      {listings.map((listing, index) => (
+        <Grid key={index} item xs={12} sm={4} sx={{position:'relative', display:'flex', justifyContent: 'center'}}>
+          <ImageBox images={listing.images}/>
         </Grid>
-    );
-};
+      ))}
+    </Grid>
+  );
+}
+
+export default BoxList;
