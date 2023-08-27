687d63e2cda43ca7dedad55e965905280a44c3d3
Samm Kohistani <sk@apples-MacBook-Pro.local>
2023-08-27 22:50:07 +0100

    add login and register

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from './header/Header.js'
 import Properties from './Components/Properties/Properties.js'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Components/Home/Home';
+import RegisterForm from './Components/Register/Register.js';
+import Login from './Components/Login/Login';
 
 
 function App() {
@@ -15,8 +17,9 @@ function App() {
       </div>
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/properties" element={<Properties />}>
-        </Route>
+        <Route path="/properties" element={<Properties />}/>
+        <Route path="/register" element={<RegisterForm/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import { Paper, TextField, Button } from '@mui/material';
+import {useNavigate} from 'react-router-dom';
+
+export default function Login() {
+    const naviate = useNavigate();
+
+    const [successMessage, setSuccessMessage] = useState("");
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Email: " + formData.email + " Password: " + formData.password);
+        setTimeout(() => {
+            setSuccessMessage("Successfully logged in!");
+            naviate('/');
+        }, 1000);
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+      };
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        '& > :not(style)': {
+          m: 1,
+          width: '50%',
+          height: '25%',
+          position:'absolute',
+          top:'200px'
+        },
+      }}
+    >
+ <Paper elevation={3} style={{ padding: '20px' }}>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="Email"
+          name="email"
+          type="email"
+          value={formData.email}
+          onChange={handleChange}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Password"
+          name="password"
+          type="password"
+          value={formData.password}
+          onChange={handleChange}
+          fullWidth
+          margin="normal"
+        />
+        <Button type="submit" variant="contained" color="primary" style={{ marginTop: '20px' }}>
+          Login
+        </Button>
+        {successMessage && <p style={{color: 'green'}}>{successMessage}</p>}
+      </form>
+    </Paper>
+    </Box>
+  );
+}
